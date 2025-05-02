88ee2178dd74969e32e4a8bc1f3eb7820c8aa749
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-02 18:36:21 +0200

    doing my first line of react and setting it up

---
+++ 2.react-basics/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+    console.log('hello world!'); 
+    
+    //i can write javascript before the return statement
+    return ( //to make it work i NEED a return statement 
+        <div>
+            <p>Banner in react</p> 
+        </div>
+    )
+};
+
+export default App;
