cc60531dbdc5d35f0a1962e248c4f4b244e2506d
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-02 19:52:50 +0200

    doing my first click events, both function reference and inline

--- 2.react-basics/src/App.tsx
+++ 2.react-basics/src/App.tsx
@@ -5,6 +5,12 @@ function App() {
     const title: string = 'welcome to my website!';
     const subtitle: string = 'this is a website for learning react';
     const adress: string= 'https://react.dev/';
+
+    const handleClick = (e: any) => { //handle + event is industry standard
+        console.log('You clicked the button!');
+        console.log('the sites title is: ', title);
+        console.log(e);//get the event object for the button that was pressed
+    };
     
     //i can write javascript before the return statement
     return ( //to make it work i NEED a return statement 
@@ -14,7 +20,12 @@ function App() {
             <p>{subtitle}</p>
             <p>Todays date is: {new Date().toDateString()}</p>
             <p>Random number: {Math.random() * 10}</p>
-            <a href={adress}>Visit the react page!</a>
+            <a href={adress}>Visit the react page!</a> <br></br>
+
+            <button onClick={handleClick}>Click me!</button> {/* onClick is an javascript event, on + event for react */}
+            <button onClick={() => {
+                console.log('hello from function reference button!');
+            }}>This is a function reference button</button>
 
             <p>Banner in react</p> 
         </div>
