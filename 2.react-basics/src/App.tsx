2b961d1047dd2dc7780b08f61e896eb51e097260
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-02 19:38:18 +0200

    adding small comments

--- 2.react-basics/src/App.tsx
+++ 2.react-basics/src/App.tsx
@@ -4,7 +4,8 @@ function App() {
     
     //i can write javascript before the return statement
     return ( //to make it work i NEED a return statement 
-        <div>
+        //when returning JSX you can ONLY have one root element!!
+        <div className="classname"> {/* class = className in JSX, there are more differences but this is the biggest one */}
             <p>Banner in react</p> 
         </div>
     )
