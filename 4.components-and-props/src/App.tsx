e38c51d67365e546bad3a2e369f5a33cfbdb421d
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-05 13:52:19 +0200

    copying folder 3 to folder 4

---
+++ 4.components-and-props/src/App.tsx
@@ -0,0 +1,90 @@
+// all react hooks start with "use"
+// useState hooks is for components that can change over time or in a reaction to something
+import { useState } from "react";
+
+interface Event {
+    title: string,
+    id: number,
+}
+
+function App() {
+    //This option changes the name in the console, but not the dom, this is because we need to use states
+    /* let name = 'lisa';
+    const handleClick = () => {
+        name = 'tova';
+        console.log(name);
+    }; */
+
+
+    const [name, setName] = useState('lisa');
+    //first one is state value, aka 'lisa'
+    //second is a function which is used to change that state value
+    //second function should always be set+name of the first value, ex    discoMode, setDiscoMode
+
+    const [events, setEvents] = useState([ //useState with an array 
+        {title: 'Lisas birthday party', id: 1},
+        {title: 'Tovas performance', id: 2},
+        {title: 'Laundry', id: 3}
+    ]);
+
+    const [showEvents, setShowEvents] = useState(true);
+    console.log(showEvents);
+    
+
+    const handleClick = (id: number) => { //id is not included in prt 1
+        //1
+        //setName('tova'); //call the function and change its value to 'tova' when button is clicked
+        //console.log(name);
+
+        //rule of thumb:
+        //when state update depends on previous state, always use callback to access previous state first
+        //then base state update on that previous value
+        setEvents((prevEvents) => { 
+            return prevEvents.filter((event) => {
+                return id !== event.id;
+            });
+        });
+
+        console.log(id);//log the id of the button pressed
+    };
+
+
+
+
+
+
+
+
+
+
+
+    return ( 
+        <div className="App"> 
+            {showEvents && (
+                <div>
+                    <button onClick={() => {setShowEvents(false)}}>Hide events</button>
+                </div>
+            )}
+            {!showEvents && (
+                <div>
+                    <button onClick={() => {setShowEvents(true)}}>Show events</button>
+                </div>
+            )}
+
+            <h1>My name is {name}</h1>
+            {/* <button onClick={handleClick}>Change name</button> handleClick 1 */}
+
+            {/*javascript when inside {} */showEvents && events.map((event, index) => { //run righthand of && if lefthand is true, called "logical and"
+                return (
+                    <div key={event.id}> {/* you need to give each item an unique key value (id) */}
+                        <h2>{index} - {event.title}</h2>
+                        <button onClick={() => handleClick(event.id)}>Delete event</button> {/* handleClick 2 */}
+                        {/* its an anonymous function, it wont be called directly, it will be called once the button is pressed */}
+                    </div>
+                );
+            })}
+        </div>
+    )
+};
+
+export default App;
