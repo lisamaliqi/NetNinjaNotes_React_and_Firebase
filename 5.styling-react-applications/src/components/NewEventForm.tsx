abb21fc93408ae9e9f1a8ab1272f74f9269885ef
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-06 14:04:53 +0200

    using onChange when adding title and date

--- 5.styling-react-applications/src/components/NewEventForm.tsx
+++ 5.styling-react-applications/src/components/NewEventForm.tsx
@@ -1,19 +1,30 @@
+import { useState } from 'react';
 import './NewEventForm.css'
 
 export default function NewEventForm() {
-  return (
-    <form className='new-event-form'>
-        <label>
-            <span>Event Title:</span>
-            <input type="text" />
-        </label>
 
-        <label>
-            <span>Event Date:</span>
-            <input type="date" />
-        </label>
-        
-        <button>Submit</button>
-    </form>
-  )
+    const [ title, setTitle ] = useState('');
+    const [ date, setDate ] = useState('');
+ 
+    // const handleChange = (e: any) => {
+    //     setTitle(e.target.value);
+    // };
+
+    return (
+        <form className='new-event-form'>
+            <label>
+                <span>Event Title:</span>
+                <input type="text" onChange={(e) => {setTitle(e.target.value)}} />
+                {/* onChange shows regular changes being done by the user */}
+            </label>
+
+            <label>
+                <span>Event Date:</span>
+                <input type="date" onChange={(e) => {setDate(e.target.value)}}/>
+            </label>
+
+            <button>Submit</button>
+            <p>title - {title}, date - {date}</p>
+        </form>
+    )
 }
