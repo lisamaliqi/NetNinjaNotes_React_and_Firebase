c7e50b996f0455ce2b82c1d5be1de9684f66f6b9
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-06 12:00:25 +0200

    demonstrating inline styleing

--- 5.styling-react-applications/src/components/Modal.tsx
+++ 5.styling-react-applications/src/components/Modal.tsx
@@ -4,9 +4,15 @@ import './Modal.css'
 export default function Modal(props: any) {
   return ReactDOM.createPortal((
     <div className='modal-backdrop'>
-        <div className="modal">
+        <div className="modal" style={{
+            border: "4px solid", 
+            borderColor: "#ff4500",
+            textAlign: "center"
+            }}> {/* adding styling into the JSZ part instead of css file */}
+
             {/* <h2>10% off code!</h2>
             <p>Use code Lajsa for 10% off next purchase</p> */}
+
             {props.children}
             <button onClick={props.toggleModal}>close</button>
         </div>
