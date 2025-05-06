a30c9d564dca42a0269d53abb3eb8db0a8604274
lisamaliqi <lisamaliqi@hotmail.com>
2025-05-06 14:11:07 +0200

    making the inputs controlled

--- 5.styling-react-applications/src/components/NewEventForm.tsx
+++ 5.styling-react-applications/src/components/NewEventForm.tsx
@@ -10,21 +10,35 @@ export default function NewEventForm() {
     //     setTitle(e.target.value);
     // };
 
+    const resetForm = () => {
+        setTitle('');
+        setDate('');
+    };
+
     return (
         <form className='new-event-form'>
             <label>
                 <span>Event Title:</span>
-                <input type="text" onChange={(e) => {setTitle(e.target.value)}} />
+                <input 
+                    type="text" 
+                    onChange={(e) => {setTitle(e.target.value)}} 
+                    value={title} 
+                />
                 {/* onChange shows regular changes being done by the user */}
             </label>
 
             <label>
                 <span>Event Date:</span>
-                <input type="date" onChange={(e) => {setDate(e.target.value)}}/>
+                <input 
+                    type="date" 
+                    onChange={(e) => {setDate(e.target.value)}} 
+                    value={date}
+                />
             </label>
 
             <button>Submit</button>
             <p>title - {title}, date - {date}</p>
+            <p onClick={resetForm}>Reset the form</p>
         </form>
     )
 }
